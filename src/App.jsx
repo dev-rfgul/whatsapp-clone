ad6e36973c8c2df4edd29f5ff3e4c7868a84da73
dev-rfgul <rfgul587@gmail.com>
2024-11-10 18:46:30 +0500

    initialized the project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+
+      <div className='bg-green-700'>{2 + 2}</div>
+      <div className='bg-green-700'>Hello World</div>
+    </>
+  )
+}
+
+export default App
