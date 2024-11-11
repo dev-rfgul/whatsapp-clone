c2889712401e435fadc36125bc38376704a4bc10
dev-rfgul <rfgul587@gmail.com>
2024-11-11 18:48:29 +0500

    installed react-router-dom as it was not installed and created an issue while deployment

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Home from './Components/Home';
 import { auth } from './Components/firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
 
+
+
 const App = () => {
   const [user] = useAuthState(auth);
 
